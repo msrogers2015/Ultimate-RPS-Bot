ae305f897882ac5681d833dd4935e42961e467f6
msrogers2015 <69293041+msrogers2015@users.noreply.github.com>
2020-12-09 17:28:41 -0500

    Data Files
    Upload mixing files for the outcome matrix and move list

---
+++ data/matrix.py
@@ -0,0 +1,36 @@
+rps3 = [
+	[['ties with',0],['covers',-1],['breaks',1]],
+	[['covers',1],['ties with',0],['cuts',-1]],
+	[['breaks',-1],['cuts',1],['ties with',0]]
+]
+
+rps5 = [
+	[['ties with',0],['covers',-1],['breaks',1],['crushes',1],['vaporizes',-1]],
+	[['covers',1],['ties with',0],['cuts',-1],['eats',-1],['disproves',1]],
+	[['breaks',-1],['cuts',1],['ties with',0],['decapitates',1],['smashes',-1]],
+	[['crushes',-1],['eats',1],['decapitates',-1],['ties with',0],['poisons',1]],
+	[['vaporizes',1],['disproves',-1],['smashes',-1],['poisons',-1],['ties with',0]]
+]
+
+rps7 = [
+	[['ties with',0],['pounds out',-1],['puts out',-1],['blows out',-1],['burns',1],['burns',1],['melts',1]],
+	[['pounds out',1],['ties with',0],['erodes',-1],['erodes',-1],['covers',-1],['crushes',1],['crushes',1]],
+	[['puts out',1],['erodes',1],['ties with',0],['evaporates',-1],['floats',-1],['absords',-1],['rust',1]],
+	[['blows out',1],['erodes',1],['evaporates',1],['ties with',0],['fans',-1],['uses',-1],['swishes',-1]],
+	[['burns',-1],['covers',1],['floats',1],['fans',1],['ties with',0],['soaks',-1],['cuts',-1]],
+	[['burns',-1],['crushes',-1],['absords',1],['uses',1],['soaks',1],['ties with',0],['cuts',-1]],
+	[['melts',-1],['crushes',-1],['rust',-1],['swishes',1],['cuts',1],['cuts',1],['ties with',0]]
+]
+
+
+rps9 = [
+	[['ties with',0],['pounds out',-1],['puts out',-1],['blows out',-1],['burns',1],['burns',1],['melts',1],['targets',-1],['burns',1]],
+	[['pounds out',1],['ties with',0],['erodes',-1],['erodes',-1],['covers',-1],['crushes',1],['crushes',1],['targets',-1],['crushes',1]],
+	[['puts out',1],['erodes',1],['ties with',0],['evaporates',-1],['floats',-1],['absords',-1],['rust',1],['rust',1],['drinks',-1]],
+	[['blows out',1],['erodes',1],['evaporates',1],['ties with',0],['fans',-1],['uses',-1],['swishes',-1],['tarnishes',1],['breathes',-1]],
+	[['burns',-1],['covers',1],['floats',1],['fans',1],['ties with',0],['soaks',-1],['cuts',-1],['outlaws',1],['writes',-1]],
+	[['burns',-1],['crushes',-1],['absords',1],['uses',1],['soaks',1],['ties with',0],['cuts',-1],['cleans',1],['cleans with',-1]],
+	[['melts',-1],['crushes',-1],['rust',-1],['swishes',1],['cuts',1],['cuts',1],['ties with',0],['outclasses',-1],['cuts',1]],
+	[['targets',1],['targets',1],['rust',-1],['tarnishes',-1],['outlaws',-1],['cleans',-1],['outclasses',1],['ties with',0],['shoots',1]],
+	[['burns',-1],['crushes',-1],['drinks',1],['breathes',1],['writes',1],['cleans with',1],['cuts',-1],['shoots',-1],['ties with',0]]
+]
---
+++ data/moves.py
@@ -0,0 +1,7 @@
+rps3 = ['Rock','Paper','Scissors']
+
+rps5 = ['Rock','Paper','Scissors','Lizard','Spock']
+
+rps7 = ['Fire','Rock','Water','Air','Paper','Sponge','Scissors']
+
+rps9 = ['Fire','Rock','Water','Air','Paper','Sponge','Scissors', 'Gun', 'Human']
